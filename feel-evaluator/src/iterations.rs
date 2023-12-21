5b8954a98b0416bb4232c108d5706bc79d1788f7
dsntk <149608688+dsntk@users.noreply.github.com>
2023-12-21 15:27:29 +0100

    Updates.

--- feel-evaluator/src/iterations.rs
+++ feel-evaluator/src/iterations.rs
@@ -35,38 +35,94 @@ struct IterationState {
 }
 
 impl IterationState {
-  ///
+  /// Creates a new iteration state for a range.
+  fn new_range(variable: Name, start: isize, end: isize) -> Self {
+    Self {
+      iterator_type: IteratorType::Range,
+      variable,
+      index: start,
+      step: if start <= end { 1 } else { -1 },
+      start,
+      end,
+      values: None,
+    }
+  }
+
+  /// Creates a new iteration state for a list of values.
+  fn new_list(variable: Name, values: Values) -> Self {
+    Self {
+      iterator_type: IteratorType::List,
+      variable,
+      index: 0,
+      step: 1,
+      start: 0,
+      end: (values.len() as isize) - 1,
+      values: Some(values),
+    }
+  }
+
+  /// Creates a new iteration state for bound variable.
+  fn new_variable(variable: Name, bound_variable: Name) -> Self {
+    Self {
+      iterator_type: IteratorType::Variable(bound_variable),
+      variable,
+      index: 0,
+      step: 1,
+      start: 0,
+      end: 0,
+      values: None,
+    }
+  }
+
+  /// If the iterator has more values, then move to next one.
   fn next(&mut self) -> bool {
     match self.step.cmp(&0) {
       Ordering::Greater => {
         if self.index + self.step <= self.end {
           self.index += self.step;
-          false
+          true
         } else {
           self.index = self.start;
-          true
+          false
         }
       }
       Ordering::Less => {
         if self.index + self.step >= self.end {
           self.index += self.step;
-          false
+          true
         } else {
           self.index = self.start;
-          true
+          false
         }
       }
       Ordering::Equal => panic!("iteration step must not be zero"),
     }
   }
 
-  ///
+  /// Returns `true` when the iterator has more values to iterate over.
   fn has_next(&self) -> bool {
     match self.step.cmp(&0) {
       Ordering::Greater => self.index + self.step <= self.end,
       Ordering::Less => self.index + self.step >= self.end,
       Ordering::Equal => panic!("iteration step must not be zero"),
     }
+  }
+
+  ///
+  fn get_value(&self) -> Option<Value> {
+    match self.iterator_type {
+      IteratorType::Range => return Some(Value::Number(self.index.into())),
+      IteratorType::List => {
+        if let Some(values) = &self.values {
+          let index = self.index as usize;
+          if let Some(value) = values.get(index) {
+            return Some(value.clone());
+          }
+        }
+      }
+      _ => {}
+    }
+    None
   }
 }
 
@@ -79,41 +135,17 @@ pub(crate) struct FeelIterator {
 impl FeelIterator {
   ///
   pub fn add_range(&mut self, variable: Name, start: isize, end: isize) {
-    self.states.push(IterationState {
-      iterator_type: IteratorType::Range,
-      variable,
-      index: start,
-      step: if start <= end { 1 } else { -1 },
-      start,
-      end,
-      values: None,
-    });
+    self.states.push(IterationState::new_range(variable, start, end));
   }
 
   ///
   pub fn add_list(&mut self, variable: Name, values: Values) {
-    self.states.push(IterationState {
-      iterator_type: IteratorType::List,
-      variable,
-      index: 0,
-      step: 1,
-      start: 0,
-      end: (values.len() as isize) - 1,
-      values: Some(values),
-    });
+    self.states.push(IterationState::new_list(variable, values));
   }
 
   ///
-  pub fn add_variable(&mut self, variable: Name, binding: Name) {
-    self.states.push(IterationState {
-      iterator_type: IteratorType::Variable(binding),
-      variable,
-      index: 0,
-      step: 1,
-      start: 0,
-      end: 0,
-      values: None,
-    });
+  pub fn add_variable(&mut self, variable: Name, bound_variable: Name) {
+    self.states.push(IterationState::new_variable(variable, bound_variable));
   }
 
   /// Iterates over all iteration states.
@@ -124,38 +156,29 @@ impl FeelIterator {
     if self.states.is_empty() {
       return;
     }
-    let mut iteration_context = FeelContext::new();
+    let mut ctx = FeelContext::new();
     self.states.reverse();
     loop {
-      let mut is_empty_iteration = true;
-      for iteration_state in &mut self.states {
-        match &iteration_state.iterator_type {
-          IteratorType::Range => {
-            let value = Value::Number(iteration_state.index.into());
-            iteration_context.set_entry(&iteration_state.variable, value);
-            is_empty_iteration = false;
-          }
-          IteratorType::List => {
-            if let Some(values) = &iteration_state.values {
-              let index = iteration_state.index as usize;
-              if let Some(value) = values.get(index) {
-                iteration_context.set_entry(&iteration_state.variable, value.clone());
-                is_empty_iteration = false;
-              }
-            }
-          }
-          _ => {}
+      let mut empty_iteration = true;
+      for state in &mut self.states {
+        if let Some(value) = state.get_value() {
+          ctx.set_entry(&state.variable, value);
+          empty_iteration = false;
         }
       }
-      if !is_empty_iteration {
-        handler(&iteration_context);
+      if !empty_iteration {
+        handler(&ctx);
       }
       let last_state = self.states.len() - 1;
-      for (i, iteration_state) in self.states.iter_mut().enumerate() {
-        if i == last_state && !iteration_state.has_next() {
+      for (i, state) in self.states.iter_mut().enumerate() {
+        if i == last_state && !state.has_next() {
+          // the last value of the outermost iterator is reached,
+          // iterating is completed, exit the whole function
           return;
         }
-        if !iteration_state.next() {
+        if state.next() {
+          // current iterator has more values to process,
+          // so do not continue with other iterators
           break;
         }
       }
