50e18bdab20671bafd424c5cfc3cfbd39ede41a6
dsntk <149608688+dsntk@users.noreply.github.com>
2023-12-27 20:33:43 +0100

    Added tests.

--- feel-evaluator/src/tests/bifs/bif_round_up.rs
+++ feel-evaluator/src/tests/bifs/bif_round_up.rs
@@ -13,60 +13,190 @@ fn _0002() {
 
 #[test]
 fn _0003() {
-  te_number(false, &scope!(), "round up(5.5, 0)", 6, 0);
+  te_number(false, &scope!(), "round up(5.2, 0)", 6, 0);
 }
 
 #[test]
 fn _0004() {
-  te_number(false, &scope!(), "round up(5.6, 0)", 6, 0);
+  te_number(false, &scope!(), "round up(5.3, 0)", 6, 0);
 }
 
 #[test]
 fn _0005() {
-  te_number(false, &scope!(), "round up(5.9, 0)", 6, 0);
+  te_number(false, &scope!(), "round up(5.4, 0)", 6, 0);
 }
 
 #[test]
 fn _0006() {
-  te_number(false, &scope!(), "round up(-5.0, 0)", -5, 0);
+  te_number(false, &scope!(), "round up(5.5, 0)", 6, 0);
 }
 
 #[test]
 fn _0007() {
-  te_number(false, &scope!(), "round up(-5.1, 0)", -6, 0);
+  te_number(false, &scope!(), "round up(5.6, 0)", 6, 0);
 }
 
 #[test]
 fn _0008() {
-  te_number(false, &scope!(), "round up(-5.5, 0)", -6, 0);
+  te_number(false, &scope!(), "round up(5.7, 0)", 6, 0);
 }
 
 #[test]
 fn _0009() {
-  te_number(false, &scope!(), "round up(-5.6, 0)", -6, 0);
+  te_number(false, &scope!(), "round up(5.8, 0)", 6, 0);
 }
 
 #[test]
 fn _0010() {
-  te_number(false, &scope!(), "round up(-5.9, 0)", -6, 0);
+  te_number(false, &scope!(), "round up(5.9, 0)", 6, 0);
 }
 
 #[test]
 fn _0011() {
-  te_number(false, &scope!(), "round up(1.121, 2)", 113, 2);
+  te_number(false, &scope!(), "round up(-5.0, 0)", -5, 0);
 }
 
 #[test]
 fn _0012() {
-  te_number(false, &scope!(), "round up(1.126, 2)", 113, 2);
+  te_number(false, &scope!(), "round up(-5.1, 0)", -6, 0);
 }
 
 #[test]
 fn _0013() {
-  te_number(false, &scope!(), "round up(-1.121, 2)", -113, 2);
+  te_number(false, &scope!(), "round up(-5.2, 0)", -6, 0);
 }
 
 #[test]
 fn _0014() {
+  te_number(false, &scope!(), "round up(-5.3, 0)", -6, 0);
+}
+
+#[test]
+fn _0015() {
+  te_number(false, &scope!(), "round up(-5.4, 0)", -6, 0);
+}
+
+#[test]
+fn _0016() {
+  te_number(false, &scope!(), "round up(-5.5, 0)", -6, 0);
+}
+
+#[test]
+fn _0017() {
+  te_number(false, &scope!(), "round up(-5.6, 0)", -6, 0);
+}
+
+#[test]
+fn _0018() {
+  te_number(false, &scope!(), "round up(-5.7, 0)", -6, 0);
+}
+
+#[test]
+fn _0019() {
+  te_number(false, &scope!(), "round up(-5.8, 0)", -6, 0);
+}
+
+#[test]
+fn _0020() {
+  te_number(false, &scope!(), "round up(-5.9, 0)", -6, 0);
+}
+
+#[test]
+fn _0021() {
+  te_number(false, &scope!(), "round up(1.120, 2)", 112, 2);
+}
+
+#[test]
+fn _0022() {
+  te_number(false, &scope!(), "round up(1.121, 2)", 113, 2);
+}
+
+#[test]
+fn _0023() {
+  te_number(false, &scope!(), "round up(1.122, 2)", 113, 2);
+}
+
+#[test]
+fn _0024() {
+  te_number(false, &scope!(), "round up(1.123, 2)", 113, 2);
+}
+
+#[test]
+fn _0025() {
+  te_number(false, &scope!(), "round up(1.124, 2)", 113, 2);
+}
+
+#[test]
+fn _0026() {
+  te_number(false, &scope!(), "round up(1.125, 2)", 113, 2);
+}
+
+#[test]
+fn _0027() {
+  te_number(false, &scope!(), "round up(1.126, 2)", 113, 2);
+}
+
+#[test]
+fn _0028() {
+  te_number(false, &scope!(), "round up(1.127, 2)", 113, 2);
+}
+
+#[test]
+fn _0029() {
+  te_number(false, &scope!(), "round up(1.128, 2)", 113, 2);
+}
+
+#[test]
+fn _0030() {
+  te_number(false, &scope!(), "round up(1.129, 2)", 113, 2);
+}
+
+#[test]
+fn _0031() {
+  te_number(false, &scope!(), "round up(-1.120, 2)", -112, 2);
+}
+
+#[test]
+fn _0032() {
+  te_number(false, &scope!(), "round up(-1.121, 2)", -113, 2);
+}
+
+#[test]
+fn _0033() {
+  te_number(false, &scope!(), "round up(-1.122, 2)", -113, 2);
+}
+
+#[test]
+fn _0034() {
+  te_number(false, &scope!(), "round up(-1.123, 2)", -113, 2);
+}
+
+#[test]
+fn _0035() {
+  te_number(false, &scope!(), "round up(-1.124, 2)", -113, 2);
+}
+
+#[test]
+fn _0036() {
+  te_number(false, &scope!(), "round up(-1.125, 2)", -113, 2);
+}
+
+#[test]
+fn _0037() {
   te_number(false, &scope!(), "round up(-1.126, 2)", -113, 2);
+}
+
+#[test]
+fn _0038() {
+  te_number(false, &scope!(), "round up(-1.127, 2)", -113, 2);
+}
+
+#[test]
+fn _0039() {
+  te_number(false, &scope!(), "round up(-1.128, 2)", -113, 2);
+}
+
+#[test]
+fn _0040() {
+  te_number(false, &scope!(), "round up(-1.129, 2)", -113, 2);
 }
