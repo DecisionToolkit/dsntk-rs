e857730d078f0cae7ca4dcbfd233a48754e105f8
dsntk <149608688+dsntk@users.noreply.github.com>
2023-12-30 16:26:14 +0100

    Updates.

--- model-evaluator/benches/compatibility/level_3/dmn_3_0103.rs
+++ model-evaluator/benches/compatibility/level_3/dmn_3_0103.rs
@@ -3,352 +3,402 @@ use super::*;
 from_examples!(DMN_3_0103);
 static_context!(CTX, "{}");
 
-#[test]
-fn _0001() {
+#[bench]
+fn _0001(b: &mut Bencher) {
   let invocable = "number_001";
   let expected = "true";
   assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX, expected);
+  iter!(b, MODEL_EVALUATOR.evaluate_invocable(&MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX));
 }
 
-#[test]
-fn _0002() {
+#[bench]
+fn _0002(b: &mut Bencher) {
   let invocable = "number_002";
   let expected = "false";
   assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX, expected);
+  iter!(b, MODEL_EVALUATOR.evaluate_invocable(&MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX));
 }
 
-#[test]
-fn _0003() {
+#[bench]
+fn _0003(b: &mut Bencher) {
   let invocable = "boolean_001";
   let expected = "true";
   assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX, expected);
+  iter!(b, MODEL_EVALUATOR.evaluate_invocable(&MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX));
 }
 
-#[test]
-fn _0004() {
+#[bench]
+fn _0004(b: &mut Bencher) {
   let invocable = "boolean_002";
   let expected = "false";
   assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX, expected);
+  iter!(b, MODEL_EVALUATOR.evaluate_invocable(&MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX));
 }
 
-#[test]
-fn _0005() {
+#[bench]
+fn _0005(b: &mut Bencher) {
   let invocable = "string_001";
   let expected = "true";
   assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX, expected);
+  iter!(b, MODEL_EVALUATOR.evaluate_invocable(&MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX));
 }
 
-#[test]
-fn _0006() {
+#[bench]
+fn _0006(b: &mut Bencher) {
   let invocable = "string_002";
   let expected = "false";
   assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX, expected);
+  iter!(b, MODEL_EVALUATOR.evaluate_invocable(&MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX));
 }
 
-#[test]
-fn _0007() {
+#[bench]
+fn _0007(b: &mut Bencher) {
   let invocable = "date_001";
   let expected = "true";
   assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX, expected);
+  iter!(b, MODEL_EVALUATOR.evaluate_invocable(&MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX));
 }
 
-#[test]
-fn _0008() {
+#[bench]
+fn _0008(b: &mut Bencher) {
   let invocable = "date_002";
   let expected = "false";
   assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX, expected);
+  iter!(b, MODEL_EVALUATOR.evaluate_invocable(&MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX));
 }
 
-#[test]
-fn _0009() {
+#[bench]
+fn _0009(b: &mut Bencher) {
   let invocable = "date_003";
   let expected = "false";
   assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX, expected);
+  iter!(b, MODEL_EVALUATOR.evaluate_invocable(&MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX));
 }
 
-#[test]
-fn _0010() {
+#[bench]
+fn _0010(b: &mut Bencher) {
   let invocable = "date_004";
   let expected = "false";
   assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX, expected);
+  iter!(b, MODEL_EVALUATOR.evaluate_invocable(&MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX));
 }
 
-#[test]
-fn _0011() {
+#[bench]
+fn _0011(b: &mut Bencher) {
   let invocable = "date_005";
   let expected = "false";
   assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX, expected);
+  iter!(b, MODEL_EVALUATOR.evaluate_invocable(&MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX));
 }
 
-#[test]
-fn _0012() {
+#[bench]
+fn _0012(b: &mut Bencher) {
   let invocable = "time_001";
   let expected = "true";
   assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX, expected);
+  iter!(b, MODEL_EVALUATOR.evaluate_invocable(&MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX));
 }
 
-#[test]
-fn _0013() {
+#[bench]
+fn _0013(b: &mut Bencher) {
   let invocable = "time_002";
   let expected = "true";
   assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX, expected);
+  iter!(b, MODEL_EVALUATOR.evaluate_invocable(&MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX));
 }
 
-#[test]
-fn _0014() {
+#[bench]
+fn _0014(b: &mut Bencher) {
   let invocable = "time_003";
   let expected = "true";
   assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX, expected);
+  iter!(b, MODEL_EVALUATOR.evaluate_invocable(&MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX));
 }
 
-#[test]
-fn _0015() {
+#[bench]
+fn _0015(b: &mut Bencher) {
   let invocable = "time_004";
   let expected = "true";
   assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX, expected);
+  iter!(b, MODEL_EVALUATOR.evaluate_invocable(&MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX));
 }
 
-#[test]
-fn _0016() {
+#[bench]
+fn _0016(b: &mut Bencher) {
   let invocable = "time_005";
   let expected = "false";
   assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX, expected);
+  iter!(b, MODEL_EVALUATOR.evaluate_invocable(&MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX));
 }
 
-#[test]
-fn _0017() {
+#[bench]
+fn _0017(b: &mut Bencher) {
   let invocable = "time_006";
   let expected = "false";
   assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX, expected);
+  iter!(b, MODEL_EVALUATOR.evaluate_invocable(&MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX));
 }
 
-#[test]
-fn _0018() {
+#[bench]
+fn _0018(b: &mut Bencher) {
   let invocable = "time_007";
   let expected = "false";
   assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX, expected);
+  iter!(b, MODEL_EVALUATOR.evaluate_invocable(&MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX));
 }
 
-#[test]
-fn _0019() {
+#[bench]
+fn _0019(b: &mut Bencher) {
   let invocable = "time_008";
   let expected = "false";
   assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX, expected);
+  iter!(b, MODEL_EVALUATOR.evaluate_invocable(&MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX));
 }
 
-#[test]
-fn _0020() {
+#[bench]
+fn _0020(b: &mut Bencher) {
   let invocable = "time_009";
   let expected = "false";
   assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX, expected);
+  iter!(b, MODEL_EVALUATOR.evaluate_invocable(&MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX));
 }
 
-#[test]
-fn _0021() {
+#[bench]
+fn _0021(b: &mut Bencher) {
   let invocable = "time_010";
   let expected = "false";
   assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX, expected);
+  iter!(b, MODEL_EVALUATOR.evaluate_invocable(&MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX));
 }
 
-#[test]
-fn _0022() {
+#[bench]
+fn _0022(b: &mut Bencher) {
   let invocable = "time_011";
   let expected = "false";
   assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX, expected);
+  iter!(b, MODEL_EVALUATOR.evaluate_invocable(&MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX));
 }
 
-#[test]
-fn _0023() {
+#[bench]
+fn _0023(b: &mut Bencher) {
   let invocable = "time_012";
   let expected = "false";
   assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX, expected);
+  iter!(b, MODEL_EVALUATOR.evaluate_invocable(&MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX));
 }
 
-#[test]
-fn _0024() {
+#[bench]
+fn _0024(b: &mut Bencher) {
   let invocable = "time_013";
   let expected = "false";
   assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX, expected);
+  iter!(b, MODEL_EVALUATOR.evaluate_invocable(&MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX));
 }
 
-#[test]
-fn _0025() {
+#[bench]
+fn _0025(b: &mut Bencher) {
   let invocable = "datetime_001";
   let expected = "true";
   assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX, expected);
+  iter!(b, MODEL_EVALUATOR.evaluate_invocable(&MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX));
 }
 
-#[test]
-fn _0026() {
+#[bench]
+fn _0026(b: &mut Bencher) {
   let invocable = "datetime_002";
   let expected = "true";
   assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX, expected);
+  iter!(b, MODEL_EVALUATOR.evaluate_invocable(&MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX));
 }
 
-#[test]
-fn _0027() {
+#[bench]
+fn _0027(b: &mut Bencher) {
   let invocable = "datetime_003";
   let expected = "true";
   assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX, expected);
+  iter!(b, MODEL_EVALUATOR.evaluate_invocable(&MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX));
 }
 
-#[test]
-fn _0028() {
+#[bench]
+fn _0028(b: &mut Bencher) {
   let invocable = "datetime_004";
   let expected = "false";
   assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX, expected);
+  iter!(b, MODEL_EVALUATOR.evaluate_invocable(&MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX));
 }
 
-#[test]
-fn _0029() {
+#[bench]
+fn _0029(b: &mut Bencher) {
   let invocable = "datetime_005";
   let expected = "false";
   assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX, expected);
+  iter!(b, MODEL_EVALUATOR.evaluate_invocable(&MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX));
 }
 
-#[test]
-fn _0030() {
+#[bench]
+fn _0030(b: &mut Bencher) {
   let invocable = "datetime_006";
   let expected = "false";
   assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX, expected);
+  iter!(b, MODEL_EVALUATOR.evaluate_invocable(&MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX));
 }
 
-#[test]
-fn _0031() {
+#[bench]
+fn _0031(b: &mut Bencher) {
   let invocable = "datetime_007";
   let expected = "false";
   assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX, expected);
+  iter!(b, MODEL_EVALUATOR.evaluate_invocable(&MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX));
 }
 
-#[test]
-fn _0032() {
+#[bench]
+fn _0032(b: &mut Bencher) {
   let invocable = "datetime_008";
   let expected = "false";
   assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX, expected);
+  iter!(b, MODEL_EVALUATOR.evaluate_invocable(&MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX));
 }
 
-#[test]
-fn _0033() {
+#[bench]
+fn _0033(b: &mut Bencher) {
   let invocable = "datetime_009";
   let expected = "false";
   assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX, expected);
+  iter!(b, MODEL_EVALUATOR.evaluate_invocable(&MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX));
 }
 
-#[test]
-fn _0034() {
+#[bench]
+fn _0034(b: &mut Bencher) {
   let invocable = "datetime_010";
   let expected = "false";
   assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX, expected);
+  iter!(b, MODEL_EVALUATOR.evaluate_invocable(&MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX));
 }
 
-#[test]
-fn _0035() {
+#[bench]
+fn _0035(b: &mut Bencher) {
   let invocable = "datetime_011";
   let expected = "false";
   assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX, expected);
+  iter!(b, MODEL_EVALUATOR.evaluate_invocable(&MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX));
 }
 
-#[test]
-fn _0036() {
+#[bench]
+fn _0036(b: &mut Bencher) {
   let invocable = "datetime_012";
   let expected = "false";
   assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX, expected);
+  iter!(b, MODEL_EVALUATOR.evaluate_invocable(&MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX));
 }
 
-#[test]
-fn _0037() {
+#[bench]
+fn _0037(b: &mut Bencher) {
   let invocable = "datetime_013";
   let expected = "false";
   assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX, expected);
+  iter!(b, MODEL_EVALUATOR.evaluate_invocable(&MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX));
 }
 
-#[test]
-fn _0038() {
+#[bench]
+fn _0038(b: &mut Bencher) {
   let invocable = "datetime_014";
   let expected = "false";
   assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX, expected);
+  iter!(b, MODEL_EVALUATOR.evaluate_invocable(&MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX));
 }
 
-#[test]
-fn _0039() {
+#[bench]
+fn _0039(b: &mut Bencher) {
   let invocable = "datetime_015";
   let expected = "true";
   assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX, expected);
+  iter!(b, MODEL_EVALUATOR.evaluate_invocable(&MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX));
 }
 
-#[test]
-fn _0040() {
+#[bench]
+fn _0040(b: &mut Bencher) {
   let invocable = "datetime_016";
   let expected = "false";
   assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX, expected);
+  iter!(b, MODEL_EVALUATOR.evaluate_invocable(&MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX));
 }
 
-#[test]
-fn _0041() {
+#[bench]
+fn _0041(b: &mut Bencher) {
   let invocable = "datetime_017";
   let expected = "false";
   assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX, expected);
+  iter!(b, MODEL_EVALUATOR.evaluate_invocable(&MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX));
 }
 
-#[test]
-fn _0042() {
+#[bench]
+fn _0042(b: &mut Bencher) {
   let invocable = "dt_duration_001";
   let expected = "true";
   assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX, expected);
+  iter!(b, MODEL_EVALUATOR.evaluate_invocable(&MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX));
 }
 
-#[test]
-fn _0043() {
+#[bench]
+fn _0043(b: &mut Bencher) {
   let invocable = "dt_duration_002";
   let expected = "true";
   assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX, expected);
+  iter!(b, MODEL_EVALUATOR.evaluate_invocable(&MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX));
 }
 
-#[test]
-fn _0044() {
+#[bench]
+fn _0044(b: &mut Bencher) {
   let invocable = "ym_duration_001";
   let expected = "true";
   assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX, expected);
+  iter!(b, MODEL_EVALUATOR.evaluate_invocable(&MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX));
 }
 
-#[test]
-fn _0045() {
+#[bench]
+fn _0045(b: &mut Bencher) {
   let invocable = "ym_duration_002";
   let expected = "true";
   assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX, expected);
+  iter!(b, MODEL_EVALUATOR.evaluate_invocable(&MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX));
 }
 
-#[test]
-fn _0046() {
+#[bench]
+fn _0046(b: &mut Bencher) {
   let invocable = "zero_duration_001";
   let expected = "false";
   assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX, expected);
+  iter!(b, MODEL_EVALUATOR.evaluate_invocable(&MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX));
 }
 
-#[test]
-fn _0047() {
+#[bench]
+fn _0047(b: &mut Bencher) {
   let invocable = "named_params_001";
   let expected = "true";
   assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX, expected);
+  iter!(b, MODEL_EVALUATOR.evaluate_invocable(&MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX));
 }
 
-#[test]
-fn _0048() {
+#[bench]
+fn _0048(b: &mut Bencher) {
   let invocable = "named_params_002";
   let expected = "false";
   assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX, expected);
+  iter!(b, MODEL_EVALUATOR.evaluate_invocable(&MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX));
 }
 
-#[test]
-fn _0049() {
+#[bench]
+fn _0049(b: &mut Bencher) {
   let invocable = "named_params_003";
   let expected = "false";
   assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX, expected);
+  iter!(b, MODEL_EVALUATOR.evaluate_invocable(&MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX));
 }
 
-#[test]
-fn _0050() {
+#[bench]
+fn _0050(b: &mut Bencher) {
   let invocable = "named_params_004";
   let expected = "null(expected 2 parameters, actual number of parameters is 3)";
   assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX, expected);
+  iter!(b, MODEL_EVALUATOR.evaluate_invocable(&MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX));
 }
