86769bc759bee09a2d78cf03bd9aac58318ea412
dsntk <149608688+dsntk@users.noreply.github.com>
2023-12-30 13:09:05 +0100

    Updates.

---
+++ model-evaluator/src/tests/compatibility/level_3/dmn_3_1142.rs
@@ -0,0 +1,9 @@
+use super::*;
+
+from_examples!(DMN_3_1142);
+static_context!(CTX, "{}");
+
+#[test]
+fn _0001() {
+  assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, "decision001", &CTX, "5");
+}
