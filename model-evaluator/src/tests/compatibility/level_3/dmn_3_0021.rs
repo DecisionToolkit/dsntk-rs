ecd6a03ac7c3a2cd8ff30ae8e909321d939a165b
dsntk <149608688+dsntk@users.noreply.github.com>
2023-12-30 16:44:46 +0100

    Updates.

--- model-evaluator/src/tests/compatibility/level_3/dmn_3_0021.rs
+++ model-evaluator/src/tests/compatibility/level_3/dmn_3_0021.rs
@@ -1,39 +1,46 @@
 use super::*;
 
 from_examples!(DMN_3_0021);
+static CTX: Lazy<FeelContext> = Lazy::new(|| context(r#"{Employees: ["Jack","John","Bob","Zack"]}"#));
 
 #[test]
 fn _0001() {
-  let ctx = context(r#"{Employees: ["Jack","John","Bob","Zack"]}"#);
-  assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, "decision1", &ctx, r#"["John"]"#);
+  let invocable = "decision1";
+  let expected = r#"["John"]"#;
+  assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX, expected);
 }
 
 #[test]
 fn _0002() {
-  let ctx = context(r#"{Employees: ["Jack","John","Bob","Zack"]}"#);
-  assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, "decision2", &ctx, r#""John""#);
+  let invocable = "decision2";
+  let expected = r#""John""#;
+  assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX, expected);
 }
 
 #[test]
 fn _0003() {
-  let ctx = context(r#"{Employees: ["Jack","John","Bob","Zack"]}"#);
-  assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, "decision3", &ctx, r#"["Bob"]"#);
+  let invocable = "decision3";
+  let expected = r#"["Bob"]"#;
+  assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX, expected);
 }
 
 #[test]
 fn _0004() {
-  let ctx = context(r#"{Employees: ["Jack","John","Bob","Zack"]}"#);
-  assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, "decision4", &ctx, r#""Bob""#);
+  let invocable = "decision4";
+  let expected = r#""Bob""#;
+  assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX, expected);
 }
 
 #[test]
 fn _0005() {
-  let ctx = context(r#"{Employees: ["Jack","John","Bob","Zack"]}"#);
-  assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, "decision5", &ctx, r#""BOB""#);
+  let invocable = "decision5";
+  let expected = r#""BOB""#;
+  assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX, expected);
 }
 
 #[test]
 fn _0006() {
-  let ctx = context(r#"{Employees: ["Jack","John","Bob","Zack"]}"#);
-  assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, "decision6", &ctx, r#""bob""#);
+  let invocable = "decision6";
+  let expected = r#""bob""#;
+  assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX, expected);
 }
