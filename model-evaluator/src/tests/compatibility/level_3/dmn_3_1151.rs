988f31215550c0e293d12e312eb0bf1af65fb0c9
dsntk <149608688+dsntk@users.noreply.github.com>
2024-01-05 10:22:27 +0100

    Updates.

--- model-evaluator/src/tests/compatibility/level_3/dmn_3_1151.rs
+++ model-evaluator/src/tests/compatibility/level_3/dmn_3_1151.rs
@@ -5,6 +5,7 @@ static_context!(CTX, "{}");
 
 #[test]
 fn _0001() {
-  //TODO Implement the full functionality of DMN 1.5.
-  assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, "decision001", &CTX, "null(boxed 'filter' not implemented)");
+  let invocable_name = "decision001";
+  let expected = "[3, 4, 5]";
+  assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, invocable_name, &CTX, expected);
 }
