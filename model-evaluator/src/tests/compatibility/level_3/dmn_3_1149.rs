6513400f29e159b054699bad554a1cd7e1a0e851
dsntk <149608688+dsntk@users.noreply.github.com>
2024-01-06 10:52:46 +0100

    Updates.

--- model-evaluator/src/tests/compatibility/level_3/dmn_3_1149.rs
+++ model-evaluator/src/tests/compatibility/level_3/dmn_3_1149.rs
@@ -5,17 +5,14 @@ static_context!(CTX, "{}");
 
 #[test]
 fn _0001() {
-  assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, "decision001", &CTX, "true");
+  let invocable = "decision001";
+  let expected = "true";
+  assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX, expected);
 }
 
 #[test]
 fn _0002() {
-  assert_decision(
-    &MODEL_EVALUATOR,
-    &MODEL_NAMESPACE,
-    &MODEL_NAME,
-    "decision002",
-    &CTX,
-    "null(expected 0 parameters, actual number of parameters is 1)",
-  );
+  let invocable = "decision002";
+  let expected = "null(expected 0 parameters, actual number of parameters is 1)";
+  assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX, expected);
 }
