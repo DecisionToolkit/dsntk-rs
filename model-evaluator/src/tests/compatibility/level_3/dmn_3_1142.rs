a8732b2fcaff69e9b253e19d76522b66a408f0ad
dsntk <149608688+dsntk@users.noreply.github.com>
2024-01-06 17:18:02 +0100

    Updates.

--- model-evaluator/src/tests/compatibility/level_3/dmn_3_1142.rs
+++ model-evaluator/src/tests/compatibility/level_3/dmn_3_1142.rs
@@ -16,24 +16,28 @@ fn _0002() {
   let expected = "-5";
   assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX, expected);
 }
+
 #[test]
 fn _0003() {
   let invocable = "decision003";
   let expected = "1.12";
   assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX, expected);
 }
+
 #[test]
 fn _0004() {
   let invocable = "decision004";
   let expected = "-1.12";
   assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX, expected);
 }
+
 #[test]
 fn _0005() {
   let invocable = "decision005";
   let expected = "null(expected 2 parameters, actual number of parameters is 0)";
   assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX, expected);
 }
+
 #[test]
 #[ignore]
 fn _0006() {
@@ -41,36 +45,42 @@ fn _0006() {
   let expected = "6";
   assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX, expected);
 }
+
 #[test]
 fn _0007() {
   let invocable = "decision007";
   let expected = "null([core::round down] invalid argument type, expected number, actual type is Null)";
   assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX, expected);
 }
+
 #[test]
 fn _0008() {
   let invocable = "decision008";
   let expected = "null([core::round down] invalid argument type, expected number, actual type is Null)";
   assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX, expected);
 }
+
 #[test]
 fn _0009() {
   let invocable = "decision009";
   let expected = "null(expected 2 parameters, actual number of parameters is 3)";
   assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX, expected);
 }
+
 #[test]
 fn _0010() {
   let invocable = "decision010";
   let expected = "5";
   assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX, expected);
 }
+
 #[test]
 fn _0011() {
   let invocable = "decision011_a";
   let expected = "null(expected 2 parameters, actual number of parameters is 1)";
   assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX, expected);
 }
+
 #[test]
 #[ignore]
 fn _0012() {
@@ -78,24 +88,28 @@ fn _0012() {
   let expected = "6";
   assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX, expected);
 }
+
 #[test]
 fn _0013() {
   let invocable = "decision012";
   let expected = "null(expected 2 parameters, actual number of parameters is 3)";
   assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX, expected);
 }
+
 #[test]
 fn _0014() {
   let invocable = "decision013";
   let expected = "null([core::round down] invalid argument type, expected number, actual type is string)";
   assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX, expected);
 }
+
 #[test]
 fn _0015() {
   let invocable = "decision014";
   let expected = "null([core::round down] invalid argument type, expected number, actual type is string)";
   assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX, expected);
 }
+
 #[test]
 #[ignore]
 fn _0016() {
@@ -103,6 +117,7 @@ fn _0016() {
   let expected = "6";
   assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX, expected);
 }
+
 #[test]
 #[ignore]
 fn _0017() {
@@ -110,24 +125,28 @@ fn _0017() {
   let expected = "6";
   assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX, expected);
 }
+
 #[test]
 fn _0018() {
   let invocable = "decision016_b";
   let expected = "null([core::round_down] <FeelNumberError> invalid scale, allowed range is -6111..6144, actual is -6112)";
   assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX, expected);
 }
+
 #[test]
 fn _0019() {
   let invocable = "decision017_a";
   let expected = "5.5";
   assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX, expected);
 }
+
 #[test]
 fn _0020() {
   let invocable = "decision017_b";
   let expected = "null([core::round_down] <FeelNumberError> invalid scale, allowed range is -6111..6144, actual is 6177)";
   assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, invocable, &CTX, expected);
 }
+
 #[test]
 fn _0021() {
   let invocable = "decision017_b";
