87216a1f9ba60ffe2ae3ad2cd88ce574c02a62e5
Dariusz Depta <141360751+DariuszDepta@users.noreply.github.com>
2024-09-10 15:43:20 +0200

    Fixed colouring. (#52)

--- bbt/tests/cli/tdt/1006_ERR/run.sh
+++ bbt/tests/cli/tdt/1006_ERR/run.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
-dsntk tdt -c never input.test input.dtb 2>&1
-
-dsntk tdt input.test input.dtb 2>&1 | sed -r 's/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]//g'
+dsntk tdt -c never  input.test input.dtb 2>&1
+dsntk tdt -c always input.test input.dtb 2>&1
