aef491405ff49a31d069056e662cc5c50ca1ec97
Dariusz Depta <141360751+DariuszDepta@users.noreply.github.com>
2024-09-18 09:54:15 +0200

    Fixed crates order.

--- publish.sh
+++ publish.sh
@@ -19,23 +19,23 @@ publish() {
   echo "┌─────────────────────────────────────────────────────────────────────"
   echo "│ Published $1 ($3/18) "
   echo "└─────────────────────────────────────────────────────────────────────"
-  echo "...sleeping 5s..."
-  sleep 5s
+  echo "...sleeping 10s..."
+  sleep 10s
 }
 
 publish_all() {
-  publish "examples"          "dsntk-examples"             1
-  publish "macros"            "dsntk-macros"               2
-  publish "common"            "dsntk-common"               3
-  publish "feel-regex"        "dsntk-feel-regex"           4
-  publish "feel-number"       "dsntk-feel-number"          5
-  publish "feel-temporal"     "dsntk-feel-temporal"        6
-  publish "feel-grammar"      "dsntk-feel-grammar"         7
-  publish "feel"              "dsntk-feel"                 8
-  publish "feel-parser"       "dsntk-feel-parser"          9
-  publish "feel-evaluator"    "dsntk-feel-evaluator"      10
-  publish "model"             "dsntk-model"               11
-  publish "recognizer"        "dsntk-recognizer"          12
+#  publish "examples"          "dsntk-examples"             1
+#  publish "macros"            "dsntk-macros"               2
+#  publish "common"            "dsntk-common"               3
+#  publish "feel-regex"        "dsntk-feel-regex"           4
+#  publish "feel-number"       "dsntk-feel-number"          5
+#  publish "feel-temporal"     "dsntk-feel-temporal"        6
+#  publish "feel-grammar"      "dsntk-feel-grammar"         7
+#  publish "feel"              "dsntk-feel"                 8
+#  publish "feel-parser"       "dsntk-feel-parser"          9
+#  publish "feel-evaluator"    "dsntk-feel-evaluator"      10
+  publish "recognizer"        "dsntk-recognizer"          11
+  publish "model"             "dsntk-model"               12
   publish "model-evaluator"   "dsntk-model-evaluator"     13
   publish "evaluator"         "dsntk-evaluator"           14
   publish "gendoc"            "dsntk-gendoc"              15
