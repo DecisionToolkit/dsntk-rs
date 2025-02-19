42bcd43a4f72b98d77334932d87a1cc515bea6fd
Dariusz Depta <141360751+DariuszDepta@users.noreply.github.com>
2025-02-19 17:07:12 +0100

    Updates.

--- feel-regex/src/utils.rs
+++ feel-regex/src/utils.rs
@@ -1,3 +1,4 @@
+/// Returns `true` when the specified character is a special one in patterns.
 pub fn is_special_character(ch: char) -> bool {
   matches!(ch, '.' | '+' | '*' | '^' | '$' | '?' | '[' | ']' | '(' | ')' | '|' | '{' | '}' | '\\')
 }
