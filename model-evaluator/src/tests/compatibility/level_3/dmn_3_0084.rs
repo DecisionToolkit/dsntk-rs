ff0f0cc5c2b71db12f369b34262f5deae7dd2735
Dariusz Depta <141360751+DariuszDepta@users.noreply.github.com>
2025-02-21 11:25:09 +0100

    Added benchmarks.

--- model-evaluator/src/tests/compatibility/level_3/dmn_3_0084.rs
+++ model-evaluator/src/tests/compatibility/level_3/dmn_3_0084.rs
@@ -6,76 +6,90 @@ static_context!(CTX, r#"{}"#);
 
 #[test]
 fn _0001() {
-  assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, "decision_001", &CTX, "[2, 3, 4]");
+  let invocable_name = "decision_001";
+  assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, invocable_name, &CTX, "[2, 3, 4]");
 }
 
 #[test]
 fn _0002() {
-  assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, "decision_002", &CTX, "[5, 6, 6, 7, 7, 8]");
+  let invocable_name = "decision_002";
+  assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, invocable_name, &CTX, "[5, 6, 6, 7, 7, 8]");
 }
 
 #[test]
 fn _0003() {
-  assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, "decision_003", &CTX, "[]");
+  let invocable_name = "decision_003";
+  assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, invocable_name, &CTX, "[]");
 }
 
 #[test]
 fn _0004() {
-  assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, "decision_007", &CTX, "[2, 3, 4]");
+  let invocable_name = "decision_007";
+  assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, invocable_name, &CTX, "[2, 3, 4]");
 }
 
 #[test]
 fn _0005() {
-  assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, "decision_008", &CTX, "[4, 3, 2]");
+  let invocable_name = "decision_008";
+  assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, invocable_name, &CTX, "[4, 3, 2]");
 }
 
 #[test]
 fn _0006() {
-  assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, "decision_009", &CTX, "[-1, 0, 1]");
+  let invocable_name = "decision_009";
+  assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, invocable_name, &CTX, "[-1, 0, 1]");
 }
 
 #[test]
 fn _0007() {
-  assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, "decision_010", &CTX, "[1, 0, -1]");
+  let invocable_name = "decision_010";
+  assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, invocable_name, &CTX, "[1, 0, -1]");
 }
 
 #[test]
 fn _0008() {
-  assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, "decision_011", &CTX, "[1]");
+  let invocable_name = "decision_011";
+  assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, invocable_name, &CTX, "[1]");
 }
 
 #[test]
 fn _0009() {
-  assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, "decision_012", &CTX, "[2, 3, 4]");
+  let invocable_name = "decision_012";
+  assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, invocable_name, &CTX, "[2, 3, 4]");
 }
 
 #[test]
 fn _0010() {
-  assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, "decision_013", &CTX, "[1, 1, 2, 6, 24]");
+  let invocable_name = "decision_013";
+  assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, invocable_name, &CTX, "[1, 1, 2, 6, 24]");
 }
 
 #[test]
 fn _0011() {
-  assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, "decision_014", &CTX, "[false, true]");
+  let invocable_name = "decision_014";
+  assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, invocable_name, &CTX, "[false, true]");
 }
 
 #[test]
 fn _0012() {
-  assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, "decision_015", &CTX, "[1, 2, 3, 4]");
+  let invocable_name = "decision_015";
+  assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, invocable_name, &CTX, "[1, 2, 3, 4]");
 }
 
 #[test]
 fn _0013() {
-  assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, "decision_016", &CTX, "[[1, 2], [3, 4]]");
+  let invocable_name = "decision_016";
+  assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, invocable_name, &CTX, "[[1, 2], [3, 4]]");
 }
 
 #[test]
 fn _0014() {
+  let invocable_name = "decision_017";
   assert_decision(
     &MODEL_EVALUATOR,
     &MODEL_NAMESPACE,
     &MODEL_NAME,
-    "decision_017",
+    invocable_name,
     &CTX,
     "[1980-01-01, 1980-01-02, 1980-01-03]",
   );
@@ -83,11 +97,12 @@ fn _0014() {
 
 #[test]
 fn _0015() {
+  let invocable_name = "decision_018";
   assert_decision(
     &MODEL_EVALUATOR,
     &MODEL_NAMESPACE,
     &MODEL_NAME,
-    "decision_018",
+    invocable_name,
     &CTX,
     "[1980-01-03, 1980-01-02, 1980-01-01]",
   );
@@ -95,11 +110,12 @@ fn _0015() {
 
 #[test]
 fn _0016() {
+  let invocable_name = "decision_019";
   assert_decision(
     &MODEL_EVALUATOR,
     &MODEL_NAMESPACE,
     &MODEL_NAME,
-    "decision_019",
+    invocable_name,
     &CTX,
     "null(<FeelEvaluatorError> invalid type of interval start: string)",
   );
@@ -107,11 +123,12 @@ fn _0016() {
 
 #[test]
 fn _0017() {
+  let invocable_name = "decision_019_a";
   assert_decision(
     &MODEL_EVALUATOR,
     &MODEL_NAMESPACE,
     &MODEL_NAME,
-    "decision_019_a",
+    invocable_name,
     &CTX,
     "null(<FeelEvaluatorError> invalid type of interval start: string)",
   );
@@ -119,11 +136,12 @@ fn _0017() {
 
 #[test]
 fn _0018() {
+  let invocable_name = "decision_020";
   assert_decision(
     &MODEL_EVALUATOR,
     &MODEL_NAMESPACE,
     &MODEL_NAME,
-    "decision_020",
+    invocable_name,
     &CTX,
     "null(<FeelEvaluatorError> invalid type of interval start: date and time)",
   );
@@ -131,11 +149,12 @@ fn _0018() {
 
 #[test]
 fn _0019() {
+  let invocable_name = "decision_020_a";
   assert_decision(
     &MODEL_EVALUATOR,
     &MODEL_NAMESPACE,
     &MODEL_NAME,
-    "decision_020_a",
+    invocable_name,
     &CTX,
     "null(<FeelEvaluatorError> invalid type of interval start: date and time)",
   );
@@ -143,11 +162,12 @@ fn _0019() {
 
 #[test]
 fn _0020() {
+  let invocable_name = "decision_021";
   assert_decision(
     &MODEL_EVALUATOR,
     &MODEL_NAMESPACE,
     &MODEL_NAME,
-    "decision_021",
+    invocable_name,
     &CTX,
     "null(<FeelEvaluatorError> invalid type of interval start: time)",
   );
@@ -155,11 +175,12 @@ fn _0020() {
 
 #[test]
 fn _0021() {
+  let invocable_name = "decision_021_a";
   assert_decision(
     &MODEL_EVALUATOR,
     &MODEL_NAMESPACE,
     &MODEL_NAME,
-    "decision_021_a",
+    invocable_name,
     &CTX,
     "null(<FeelEvaluatorError> invalid type of interval start: time)",
   );
@@ -167,11 +188,12 @@ fn _0021() {
 
 #[test]
 fn _0022() {
+  let invocable_name = "decision_022";
   assert_decision(
     &MODEL_EVALUATOR,
     &MODEL_NAMESPACE,
     &MODEL_NAME,
-    "decision_022",
+    invocable_name,
     &CTX,
     "null(<FeelEvaluatorError> invalid type of interval start: days and time duration)",
   );
@@ -179,11 +201,12 @@ fn _0022() {
 
 #[test]
 fn _0023() {
+  let invocable_name = "decision_022_a";
   assert_decision(
     &MODEL_EVALUATOR,
     &MODEL_NAMESPACE,
     &MODEL_NAME,
-    "decision_022_a",
+    invocable_name,
     &CTX,
     "null(<FeelEvaluatorError> invalid type of interval start: days and time duration)",
   );
@@ -191,16 +214,18 @@ fn _0023() {
 
 #[test]
 fn _0024() {
-  assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, "decision_023", &CTX, "[1, 2]");
+  let invocable_name = "decision_023";
+  assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, invocable_name, &CTX, "[1, 2]");
 }
 
 #[test]
 fn _0025() {
+  let invocable_name = "decision_024";
   assert_decision(
     &MODEL_EVALUATOR,
     &MODEL_NAMESPACE,
     &MODEL_NAME,
-    "decision_024",
+    invocable_name,
     &CTX,
     "[1980-01-01, 1980-01-02, 1980-01-03]",
   );
@@ -208,11 +233,12 @@ fn _0025() {
 
 #[test]
 fn _0026() {
+  let invocable_name = "decision_025";
   assert_decision(
     &MODEL_EVALUATOR,
     &MODEL_NAMESPACE,
     &MODEL_NAME,
-    "decision_025",
+    invocable_name,
     &CTX,
     "null(<FeelEvaluatorError> range start must be less or equal then range end)",
   );
