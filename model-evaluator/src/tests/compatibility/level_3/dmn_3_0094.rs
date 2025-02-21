d2242c6268b0122d7a733d9b27283cba349d2e22
Dariusz Depta <141360751+DariuszDepta@users.noreply.github.com>
2025-02-21 12:59:06 +0100

    Updates.

--- model-evaluator/src/tests/compatibility/level_3/dmn_3_0094.rs
+++ model-evaluator/src/tests/compatibility/level_3/dmn_3_0094.rs
@@ -2,69 +2,73 @@ use super::*;
 
 from_examples!(DMN_3_0094);
 
-static_context!(CTX, r#"{}"#);
+static_context!(CTX, "{}");
+
+fn eq(invocable: &str, ctx: &FeelContext, expected: &str) {
+  assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, invocable, &ctx, expected);
+}
 
 #[test]
 fn _0001() {
-  assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, "decision001", &CTX, r#"5832"#);
+  eq("decision001", &CTX, r#"5832"#);
 }
 
 #[test]
 fn _0002() {
-  assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, "decision002", &CTX, r#"null"#);
+  eq("decision002", &CTX, r#"null"#);
 }
 
 #[test]
 fn _0003() {
-  assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, "decision003", &CTX, r#"null(expected 1+ parameters, actual number of parameters is 0)"#);
+  eq("decision003", &CTX, r#"null(expected 1+ parameters, actual number of parameters is 0)"#);
 }
 
 #[test]
 fn _0004() {
-  assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, "decision003_a", &CTX, r#"null([core::product] invalid argument type, expected number, actual type is Null)"#);
+  eq("decision003_a", &CTX, r#"null([core::product] invalid argument type, expected number, actual type is Null)"#);
 }
 
 #[test]
 fn _0005() {
-  assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, "decision004", &CTX, r#"null([core::product] invalid argument type, expected number, actual type is Null)"#);
+  eq("decision004", &CTX, r#"null([core::product] invalid argument type, expected number, actual type is Null)"#);
 }
 
 #[test]
 fn _0006() {
-  assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, "decision005", &CTX, r#"null([core::product] invalid argument type, expected number, actual type is string)"#);
+  eq("decision005", &CTX, r#"null([core::product] invalid argument type, expected number, actual type is string)"#);
 }
 
 #[test]
 fn _0007() {
-  assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, "decision006", &CTX, r#"18.75"#);
+  eq("decision006", &CTX, r#"18.75"#);
 }
 
 #[test]
 fn _0008() {
-  assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, "decision007", &CTX, r#"24"#);
+  eq("decision007", &CTX, r#"24"#);
 }
 
 #[test]
 fn _0009() {
-  assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, "decision008", &CTX, r#"6"#);
+  eq("decision008", &CTX, r#"6"#);
 }
 
 #[test]
 fn _0010() {
-  assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, "decision008_a", &CTX, r#"6"#);
+  eq("decision008_a", &CTX, r#"6"#);
 }
 
 #[test]
 fn _0011() {
-  assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, "decision011", &CTX, r#"5832"#);
+  eq("decision011", &CTX, r#"5832"#);
 }
 
 #[test]
 fn _0012() {
-  assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, "decision012", &CTX, r#"null(parameter 'list' not found)"#);
+  eq("decision012", &CTX, r#"null(parameter 'list' not found)"#);
 }
 
 #[test]
 fn _0013() {
-  assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, "decision013", &CTX, r#"null(parameter 'list' not found)"#);
+  eq("decision013", &CTX, r#"null(parameter 'list' not found)"#);
 }
