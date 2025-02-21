1f22d37a073fa2405adf3d8d41b04373e65850d2
Dariusz Depta <141360751+DariuszDepta@users.noreply.github.com>
2025-02-21 20:09:27 +0100

    Updates.

--- Coverage.sh
+++ Coverage.sh
@@ -1,79 +1,41 @@
 #!/usr/bin/env bash
 
-###############################################################################
-#                                                                             #
-# Dependencies:                                                               #
-#                                                                             #
-# $ sudo dnf install lcov                                                     #
-# $ rustup component add llvm-tools-preview                                   #
-# $ cargo install grcov                                                       #
-# $ cargo install htop                                                        #
-#                                                                             #
-###############################################################################
-
 WORKING_DIRECTORY=$(pwd)
-CARGO_NAME=$(grep -oE '^name = "[^"]+"' ./dsntk/Cargo.toml | grep -oE '"[^"]+"' | grep -oE '[^"]+' | tr '[:lower:]' '[:upper:]')
-CARGO_VERSION=$(grep -oE '^version = "[^"]+"' Cargo.toml | grep -oE '"[^"]+"' | grep -oE '[0-9\.]+')
 
-# clean before proceeding
-cargo clean
+# Set the environment variables needed to get coverage.
+source <(cargo llvm-cov show-env --export-prefix --no-cfg-coverage --no-cfg-coverage-nightly)
 
-# set instrumenting variables
-export CARGO_INCREMENTAL=0
-export RUSTDOCFLAGS="-Cpanic=unwind"
-export RUSTFLAGS="-Cinstrument-coverage -Ccodegen-units=1 -Copt-level=0 -Clink-dead-code=off -Coverflow-checks=off -Zpanic_abort_tests -Cpanic=unwind"
-export LLVM_PROFILE_FILE="$WORKING_DIRECTORY/target/profraw/dsntk-%p-%m.profraw"
+# Remove artifacts that may affect the coverage results.
+# This command should be called after show-env.
+cargo llvm-cov clean --workspace
 
-# set features for parsing tables
-export CARGO_FEATURE_PARSING_TABLES=1
+# Build rust binaries.
+cargo build
 
-if [ -n "$1" ]; then
-  # run tests only for specified package
-  cargo +nightly test -p "$1"
-else
-  # run all tests
-  cargo +nightly test --workspace
-  # build the binary again
-  cargo +nightly build --workspace
-  # run black-box tests
-  cd "$WORKING_DIRECTORY"/bbt || exit 1
-  ./bbt.sh
-  cd "$WORKING_DIRECTORY" || exit 1
-fi
+# Run all tests.
+cargo test
 
-# prepare output directories for coverage results
-mkdir ./target/lcov
-mkdir ./target/coverage
+# run black-box tests
+cd "$WORKING_DIRECTORY"/bbt || exit 1
+./bbt.sh
+cd "$WORKING_DIRECTORY" || exit 1
 
-# generate coverage info
-grcov . \
-     --llvm \
-     -s . \
-     --binary-path ./target/debug/ \
-     -t lcov \
-     --branch \
-     --ignore-not-existing \
-     --ignore "*cargo*" --ignore "*tests*" --ignore "*target*" \
-     -o ./target/lcov/lcov.info
+# Generate the final coverage report.
+cargo llvm-cov report --html
 
-# generate coverage report in HTML format
-genhtml -t "$CARGO_NAME v$CARGO_VERSION" -q -o ./target/coverage ./target/lcov/lcov.info
-
-# generate coverage report in PDF format
 if [ "$PDF_REPORT" != "" ]; then
-  echo ""
-  echo "Generating PDF report..."
-  htop -b -l -p A4 --margin=4mm single ./target/coverage/index.html ./target/coverage/coverage.pdf
+  # generate coverage report in PDF format
+  echo -e "\nGenerating PDF report..."
+  htop -b -l -p A4 -m 10mm -s 90% single ./target/llvm-cov/html/index.html ./target/llvm-cov/html/index.pdf
 fi
 
-# display final message
+# Display links to the coverage reports.
 echo ""
-echo "Open coverage report:"
-echo "  HTML file://$WORKING_DIRECTORY/target/coverage/index.html"
+echo -e "\e[1;32mOpen HTML report\x3A\e[0m file:///$WORKING_DIRECTORY/target/llvm-cov/html/index.html"
 if [ "$PDF_REPORT" != "" ]; then
-  echo "   PDF file://$WORKING_DIRECTORY/target/coverage/coverage.pdf"
+  echo -e "\e[1;32mOpen PDF report\x3A\e[0m file:///$WORKING_DIRECTORY/target/llvm-cov/html/index.pdf"
 fi
 echo ""
 
-# format generated code
-cargo +nightly fmt -p dsntk-feel-parser
+# Reformat code.
+cargo +nightly fmt
