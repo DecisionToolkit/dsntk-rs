229722f16c6dfdb23144080404368c1bf6b93904
Dariusz Depta <141360751+DariuszDepta@users.noreply.github.com>
2025-03-07 10:18:29 +0100

    Automated code formatting. (#94)

--- feel-parser/build.rs
+++ feel-parser/build.rs
@@ -1,9 +1,18 @@
 use dsntk_feel_grammar::lalr_rust_tables;
+use std::process::Command;
 
 fn main() {
   if std::env::var("CARGO_FEATURE_PARSING_TABLES").is_ok() {
     // generate new LALR parsing tables for FEEL grammar
     // when executed with --features=parsing-tables
     lalr_rust_tables("./src/lalr.rs");
+    // reformat the generated code
+    let status = Command::new("cargo")
+      .args(&["+nightly", "fmt", "-p", "dsntk-feel-parser"])
+      .status()
+      .expect("failed to run code formatter");
+    if !status.success() {
+      panic!("code formatter failed");
+    }
   }
 }
