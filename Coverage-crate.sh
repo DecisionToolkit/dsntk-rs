597a514292d38d5a168fef7974120863d22c8587
Dariusz Depta <141360751+DariuszDepta@users.noreply.github.com>
2025-03-07 11:03:18 +0100

    Removed rdt command. (#95)

--- Coverage-crate.sh
+++ Coverage-crate.sh
@@ -4,4 +4,4 @@
 cargo +nightly llvm-cov -p "$1" --no-cfg-coverage --no-cfg-coverage-nightly --html
 
 # Display the link to coverage report.
-echo -e "\n\e[1;32mOpen coverage report\x3A\e[0m file:///$(pwd)/target/llvm-cov/html/index.html\n"
+echo -e "\n\e[1;32mOpen coverage report\x3A\e[0m file://$(pwd)/target/llvm-cov/html/index.html\n"
