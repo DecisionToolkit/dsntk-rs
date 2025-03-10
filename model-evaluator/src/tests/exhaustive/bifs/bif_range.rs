4f064e44e0daecba35838562a5022f6bc3d36d63
Dariusz Depta <141360751+DariuszDepta@users.noreply.github.com>
2025-03-10 11:53:50 +0100

    Composed default output values (#96)

--- model-evaluator/src/tests/exhaustive/bifs/bif_range.rs
+++ model-evaluator/src/tests/exhaustive/bifs/bif_range.rs
@@ -1,23 +1,24 @@
 use super::*;
 
-from_examples!(EXHAUSTIVE_BIF_RANGE);
+from_examples!(EXHAUSTIVE_BIFS_BIF_RANGE);
+
 static_context!(CTX, "{}");
 
-fn assert(invocable_name: &str, expected: &str) {
-  assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, invocable_name, &CTX, expected);
+fn eq(invocable_name: &str, input: &FeelContext, expected: &str) {
+  assert_decision(&MODEL_EVALUATOR, &MODEL_NAMESPACE, &MODEL_NAME, invocable_name, input, expected);
 }
 
 #[test]
 fn _0001() {
-  assert("decision_0001", "[[1..3], [1..3), (1..3], (1..3), [1..3), (1..3], (1..3), [1..), (..3]]");
+  eq("decision_0001", &CTX, "[[1..3], [1..3), (1..3], (1..3), [1..3), (1..3], (1..3), [1..), (..3]]");
 }
 
 #[test]
 fn _0002() {
-  assert("decision_0002", r#"[["a".."c"]]"#);
+  eq("decision_0002", &CTX, r#"[["a".."c"]]"#);
 }
 
 #[test]
 fn _0003() {
-  assert("decision_0100", "[true, true]");
+  eq("decision_0100", &CTX, "[true, true]");
 }
---
+++ model-evaluator/src/tests/exhaustive/decision_tables/mod.rs
@@ -0,0 +1,5 @@
+use super::*;
+
+mod multiple_default_output_values;
+mod no_default_output_values;
+mod single_default_output_value;
