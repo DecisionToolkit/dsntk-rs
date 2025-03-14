4b8378f30597f3e0bf78ea5c3376d4496f38104e
Dariusz Depta <141360751+DariuszDepta@users.noreply.github.com>
2025-03-14 11:49:20 +0100

    Updates.

--- bbt/tests/server/evaluate/success/run.sh
+++ bbt/tests/server/evaluate/success/run.sh
@@ -7,5 +7,8 @@ sleep 0.1
 curl -s -d '{"Full Name":"John Doe"}' -H "Content-Type: application/json" -X POST http://0.0.0.0:22022/evaluate/org/decision-toolkit/2_0001/compliance-level-2-test-0001/Greeting%20Message
 curl -s -d '{"Monthly Salary":12000}' -H "Content-Type: application/json" -X POST http://0.0.0.0:22022/evaluate/org/decision-toolkit/2_0002/compliance-level-2-test-0002/Yearly%20Salary
 
+curl -s -d '{"Full Name":"Andy Warchol"}' -X GET http://0.0.0.0:22022/evaluate/org/decision-toolkit/2_0001/compliance-level-2-test-0001/Greeting%20Message
+curl -s -w '\n' -d '{"Monthly Salary":67000}' -X GET http://0.0.0.0:22022/evaluate/org/decision-toolkit/2_0002/compliance-level-2-test-0002/Yearly%20Salary
+
 kill -s SIGINT "$_pid"
 sleep 0.1
